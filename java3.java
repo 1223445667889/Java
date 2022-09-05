cb092dc4999a503800f9a962a0bd47b1ff2600d6
try to do better <99059814+1223445667889@users.noreply.github.com>
2022-09-05 22:24:30 +0800

    Add files via upload

---
+++ java3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class java3 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入你的姓名");
+        String name =scanner.next();
+        System.out.println("你输入的姓名是："+ name);
+        System.out.println("请输入整数:");
+        int age =scanner.nextInt();
+        System.out.println("请输入整数:");
+        double money =scanner.nextDouble();
+        System.out.println("整数,"+ age + "小数，"+money);
+        System.out.println("请输入张三三门功课的成绩：");
+        System.out.println("JAVA：");
+        int JAVA = scanner.nextInt();
+        System.out.println("PS:");
+        int PS = scanner.nextInt();
+        System.out.println("SQL");
+        int SQL = scanner.nextInt();
+        System.out.print("JAVA和SQL的差："+(JAVA-SQL));
+        System.out.print("平均值："+((JAVA+PS+SQL)/3));
+    }
+}
