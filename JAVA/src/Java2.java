0c047f6f3e53b8bf441557506ec640c4f28e27af
被敲打的键盘 <99059814+1223445667889@users.noreply.github.com>
2022-09-20 11:52:38 +0800

    Add files via upload

---
+++ JAVA/src/Java2.java
@@ -0,0 +1,18 @@
+public class Java2 {
+    public static void main(String[] args){
+        int age = 10;
+        System.out.println("年龄："+ age);
+        age = 28;
+        System.out.println("年龄："+ age);
+        int i= 10;
+        System.out.println("整数："+ i);
+        double b=3.1415926;
+        System.out.println("小数："+ b);
+        char c='偶';
+        System.out.println("字符："+ c);
+        String d="哈哈哈哈哈";
+        System.out.println("字符串："+ d);
+        System.out.println("整数："+ i + "，小数："+ b +"，字符："+ c +"，字符串："+ d);
+    }
+
+}
