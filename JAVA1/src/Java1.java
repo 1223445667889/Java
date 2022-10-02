6955de63f82c15e45b032d5951ceb4a0512a5f87
被敲打的键盘 <99059814+1223445667889@users.noreply.github.com>
2022-10-02 18:57:13 +0800

    Update Java1.java

--- JAVA1/src/Java1.java
+++ JAVA1/src/Java1.java
@@ -88,5 +88,19 @@ public class Java1 {
             }
             System.out.println();
         }
+        
+        //心
+        for(float y = 1.5f;y>-1.5f;y-=1.0f){
+            for(float x=-1.5f;x<1.5f;x+=0.05f){
+                float a = x*x+y*y-1;
+                if((a*a*a-x*x*y*y*y)<0.0f){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print("");
+                }
+            }
+            System.out.print("\t");
+        }
     }
 }
