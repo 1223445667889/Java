d194f6fa9d9933cb8bf4190f32eee1d641891d94
被敲打的键盘 <99059814+1223445667889@users.noreply.github.com>
2022-10-11 22:16:55 +0800

    Add files via upload

---
+++ JAVA3/src/gongsi/xiangmu/pet/Penguin.java
@@ -0,0 +1,36 @@
+package gongsi.xiangmu.pet;
+
+public class Penguin extends Pet{
+    public static final String SEX_MALE = "男仔";
+    public static final String SEX_FEMALE = "靓女";
+    private String sex;
+
+    public Penguin() {
+    }
+
+    public Penguin(String name, int health, int love, String sex) {
+        super(name, health, love);
+        this.sex = sex;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public void print() {
+        super.print();
+        System.out.println(",性别是："+sex);
+    }
+
+    @Override
+    public String toString() {
+        return "Penguin{" +
+                "sex='" + sex + '\'' +
+                "} " + super.toString();
+    }
+}
