8e9599cdbb5f31a7b080715c40bfdcd529d700b8
被敲打的键盘 <99059814+1223445667889@users.noreply.github.com>
2022-10-27 09:58:09 +0800

    Add files via upload

---
+++ J315JavaProject/src/javaclass3.java
@@ -0,0 +1,23 @@
+
+public class javaclass3 {
+	public static void main(String[] args) {
+		//二维数组
+		int[][] numbers = {{0,1,2},{3,4,5},{6,7,8,9}};
+		int[] numbers1[];
+		int numbers2[][];
+		numbers = new int[5][];//为5个null
+		numbers = new int[5][5];//为25个0
+		numbers = new int[][] {{1,2,3},{1,2},{4,5,6}};
+		
+		//三维数组
+		int[][][] nums = {{{1,2},{3,4},{5,6}},{{1,2},{1,4}}};
+		
+	
+		//字符串转换为整型
+		String price ="1000";
+		int n = Integer.parseInt(price);
+		
+		
+		
+	}
+}
