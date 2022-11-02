6d4f888b69a96f185f6d1b7aceb7559bd7e161a1
被敲打的键盘 <99059814+1223445667889@users.noreply.github.com>
2022-11-02 22:48:23 +0800

    Add files via upload

---
+++ J315JavaProject/src/Javaclass6.java
@@ -0,0 +1,19 @@
+
+public class Javaclass6 {
+	public static void main(String[] args) {
+		A a = new A();
+		a.login("123123","123132" );
+		
+		Person p = new Person();
+	}
+}
+
+class A{
+	public void login(String phoneNum,int ysnzhengma) {
+		
+	}
+	
+	public void login(String email,String pwd) {
+			
+	}
+}
